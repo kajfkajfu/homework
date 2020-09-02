1a826ec7d1c158f8d2c5fe3cc4f59174001556be
kajfkajfu <kajfkajfu830@gmail.com>
2020-09-02 14:02:05 +0300

    first commit

---
+++ firstHomework/homework5/Homework5Class2.java
@@ -0,0 +1,25 @@
+package firstHomework.homework5;
+
+import java.util.Scanner;
+
+public class Homework5Class2 {
+    public static void main(String[] args) {
+        String name;
+        String friend = "Вася";
+        String girlFriend = "Анастасия";
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        name = in.nextLine();
+
+        boolean checkFriend = name.equals(friend);
+        boolean checkGirlFriend = name.equals(girlFriend);
+        if (checkFriend) {
+            System.out.println("Привет");
+            System.out.println("Я тебя так долго ждал");
+        } else if (checkGirlFriend) {
+            System.out.println("Я тебя так долго ждал");
+        } else {
+            System.out.println("Добрый день, а вы кто ?");
+        }
+    }
+}
